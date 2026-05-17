7ebdce3b835f1da4786e571e615082cde126ded0
cleiber-mata <cleiber.mata@gmail.com>
2026-05-17 15:05:18 -0300

    Criação Git e GitHub

---
+++ Biblioteca/src/Biblioteca/Livro.java
@@ -0,0 +1,34 @@
+package Biblioteca;
+
+public class Livro {
+	private String titulo;
+	private String autor;
+	private int ano;
+	private boolean emprestado;
+
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getAutor() {
+		return autor;
+	}
+	public void setAutor(String autor) {
+		this.autor = autor;
+	}
+	public int getAno() {
+		return ano;
+	}
+	public void setAno(int ano) {
+		this.ano = ano;
+	}
+	public boolean isEmprestado() {
+		return emprestado;
+	}
+	public void setEmprestado(boolean emprestado) {
+		this.emprestado = emprestado;
+	}
+}
---
+++ Biblioteca/src/Biblioteca/Teste.java
@@ -0,0 +1,14 @@
+package Biblioteca;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Livro crepusculo = new Livro();
+		crepusculo.setTitulo("Crepusculo");
+		crepusculo.setAutor("Luiz Henrique");
+		crepusculo.setAno(2001);
+		crepusculo.setEmprestado(false);
+		System.out.println("Livro " + crepusculo.getTitulo() + " esta emprestado para o Aluno: " + crepusculo.getAutor());			
+	}
+}
